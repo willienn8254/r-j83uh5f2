635a824c470b6a6815658dd1730ed4500b073b33
welinton <willienn@hotmail.com>
2017-03-17 10:19:15 -0500

    se agrega funcionalidad contar caracteres

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(){
+
+	super();
+	this.state={contador: 0}
+
+
+}
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.actualizar.bind(this)}></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
+  }
+
+
+  actualizar(event){
+
+  		this.setState({
+
+  			contador: event.target.value.length
+  		})
+
   }
 }
 
